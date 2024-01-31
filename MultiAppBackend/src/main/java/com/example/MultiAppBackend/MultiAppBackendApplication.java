be72fa909fc7cb6e59f8b5c0b814a1bd97285edd
Christoph Weber <chrisweber1991@gmail.com>
2024-01-31 19:27:03 +0100

    first commit

---
+++ MultiAppBackend/src/main/java/com/example/MultiAppBackend/MultiAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MultiAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiAppBackendApplication.class, args);
+	}
+
+}
---
+++ MultiAppBackend/src/test/java/com/example/MultiAppBackend/MultiAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MultiAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
