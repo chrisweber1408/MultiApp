a431dfba99d28f8fdefbe55c82a55456863213aa
Christoph Weber <chrisweber1991@gmail.com>
2024-02-18 15:25:04 +0100

    add an item

--- MultiAppFrontend/src/app/homizer/homizer-item-mainpage/homizer-item-mainpage.component.ts
+++ MultiAppFrontend/src/app/homizer/homizer-item-mainpage/homizer-item-mainpage.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { homizerItem } from '../service/homizer.models';
+import { HomizerItem } from '../service/homizer.models';
+import { DataStorageService } from '../../shared/data-storage.service';
 
 @Component({
   selector: 'app-homizer-mainpage',
@@ -7,8 +8,16 @@ import { homizerItem } from '../service/homizer.models';
   styleUrl: './homizer-item-mainpage.component.css'
 })
 export class HomizerItemMainpageComponent {
-  homizerItem: homizerItem[] = [
-    new homizerItem('Handy', 'Iphone 6', 'assets/images/home.png', 1)
-  ]
+  homizerItem: HomizerItem = new HomizerItem('');
+
+  constructor(private dataStorageService: DataStorageService) {
+
+  }
+
+  onSaveHomizerItem(name: string, description?: string, image?: string, number?: number){
+    console.log('test')
+    const item: HomizerItem = new HomizerItem(name, description, image, number)
+    this.dataStorageService.saveHomizerItem(item)
+  }
 
 }
