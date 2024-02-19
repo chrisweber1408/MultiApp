370382e0872cb019894027156e76fb4d3d5411e6
Christoph Weber <chrisweber1991@gmail.com>
2024-02-19 12:07:04 +0100

    wip

---
+++ MultiAppBackend/src/main/java/com/example/MultiAppBackend/homizer/storageItem/StorageItemRepo.java
@@ -0,0 +1,2 @@
+package com.example.MultiAppBackend.homizer.storageItem;public interface StorageItemRepo {
+}
--- MultiAppFrontend/src/app/shared/data-storage.service.ts
+++ MultiAppFrontend/src/app/shared/data-storage.service.ts
@@ -12,7 +12,7 @@ export class DataStorageService {
 
     saveHomizerItem(item: HomizerItem) {
         console.log(item)
-        this.http.post('https://localhost:5432' ,item).subscribe(response => {
+        this.http.post('http://localhost:8080/homizer/storageItem' ,item).subscribe(response => {
             console.log(response)
         })
     }
