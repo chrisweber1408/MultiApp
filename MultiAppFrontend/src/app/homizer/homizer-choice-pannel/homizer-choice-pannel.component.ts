d07160ee1ab3ddbac92316fb51001ed4884926a0
Christoph Weber <chrisweber1991@gmail.com>
2024-02-24 12:00:19 +0100

    created component choice pannel

---
+++ MultiAppFrontend/src/app/homizer/homizer-choice-pannel/homizer-choice-pannel.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-homizer-choice-pannel',
+  templateUrl: './homizer-choice-pannel.component.html',
+  styleUrl: './homizer-choice-pannel.component.css'
+})
+export class HomizerChoicePannelComponent {
+  
+}
