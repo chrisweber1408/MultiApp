e7d45e44e19aa6e20cb0e6ea81c0c986d3ddb749
Christoph Weber <chrisweber1991@gmail.com>
2025-01-19 17:08:23 +0100

    S007: Added storage frontend

---
+++ MultiAppFrontend/src/app/homizer/pages/homizer-storage-mainpage/homizer-storage-mainpage.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {HomizerStorage} from "../../service/homizer.models";
+import {DataService} from "../../service/homizer-data.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-homizer-storage-mainpage',
+  standalone: false,
+  templateUrl: './homizer-storage-mainpage.component.html',
+  styleUrl: './homizer-storage-mainpage.component.css'
+})
+export class HomizerStorageMainpageComponent implements OnInit {
+  homizerStorage: HomizerStorage;
+
+  constructor(private dataStorage: DataService, private activatedRoute: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.loadHomizerStorages()
+  }
+
+  async loadHomizerStorages(): Promise<HomizerStorage[]> {
+    const storages = await this.dataStorage.loadHomizerStorages()
+      .catch(error => {
+        if (error.response?.status === 403) {
+          this.router.navigate(['/login']);
+        }
+      })
+    this.homizerStorage = storages;
+    return storages;
+  }
+
+}
