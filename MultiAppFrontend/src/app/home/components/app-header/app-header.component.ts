ba385abb2221ce15a062918688036ef265344acc
Christoph Weber <chrisweber1991@gmail.com>
2025-03-07 17:01:29 +0100

    S-011: Logout functionality added

--- MultiAppFrontend/src/app/home/components/app-header/app-header.component.ts
+++ MultiAppFrontend/src/app/home/components/app-header/app-header.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, Inject, Input} from '@angular/core';
+import {Router} from '@angular/router';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-header',
@@ -10,4 +12,15 @@ export class HomeHeaderComponent {
 
   @Input() title: string;
 
+  constructor(@Inject(DOCUMENT) private document: Document, private router: Router) {
+  }
+
+  isLoggedIn(): boolean {
+    return this.document.cookie.includes('jwt='); // Check if JWT cookie exists
+  }
+
+  logout() {
+    document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 UTC;'; // Delete JWT cookie
+    this.router.navigate(['/login']);
+  }
 }
