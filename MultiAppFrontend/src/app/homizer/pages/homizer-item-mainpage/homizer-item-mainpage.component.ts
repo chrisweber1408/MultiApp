9f058774411db6fce9065523cbe51580a6f20287
Christoph Weber <chrisweber1991@gmail.com>
2025-03-14 17:04:38 +0100

    S-017: Show message of empty list a bit delayed

--- MultiAppFrontend/src/app/homizer/pages/homizer-item-mainpage/homizer-item-mainpage.component.ts
+++ MultiAppFrontend/src/app/homizer/pages/homizer-item-mainpage/homizer-item-mainpage.component.ts
@@ -1,29 +1,35 @@
-import { AfterContentChecked, Component, OnChanges, OnInit } from '@angular/core';
-import { HomizerItemDto } from '../../service/homizer.models';
-import { HomizerDataService } from '../../service/homizer-data.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import {AfterContentChecked, Component, OnChanges, OnInit} from '@angular/core';
+import {HomizerItemDto} from '../../service/homizer.models';
+import {HomizerDataService} from '../../service/homizer-data.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
-    selector: 'app-homizer-mainpage',
-    templateUrl: './homizer-item-mainpage.component.html',
-    styleUrls: ['./homizer-item-mainpage.component.css'],
-    standalone: false
+  selector: 'app-homizer-mainpage',
+  templateUrl: './homizer-item-mainpage.component.html',
+  styleUrls: ['./homizer-item-mainpage.component.css'],
+  standalone: false
 })
 export class HomizerItemMainpageComponent implements OnInit {
   homizerItems: HomizerItemDto[];
+  showNoItemsMessage = false;
 
   constructor(private dataService: HomizerDataService, private router: Router) {
 
   }
 
   ngOnInit() {
-      this.loadHomizerItems();
+    this.loadHomizerItems();
+    if (!this.homizerItems) {
+      setTimeout(() => {
+        this.showNoItemsMessage = true;
+      }, 250);
+    }
   }
 
   async loadHomizerItems(): Promise<HomizerItemDto[]> {
     const items = await this.dataService.loadHomizerItems()
       .catch(error => {
-        if(error.response?.status === 403) {
+        if (error.response?.status === 403) {
           this.router.navigate(['/login'])
         }
       });
